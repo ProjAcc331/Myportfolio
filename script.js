6fabd92cfddf7802447dac098846035845f432a2
ProjAcc331 <94629264+ProjAcc331@users.noreply.github.com>
2023-10-02 20:54:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+function scrollab() {
+    window.scrollTo(0, 1000);
+    behavior: 'smooth';
+  }
+
+  function scrollhome() {
+    window.scrollTo(0, 900);
+    behavior: 'smooth';
+  }
+
+  function scrollport() {
+    window.scrollTo(0, 1500);
+    behavior: 'smooth';
+  }
+
+  function scrollskill() {
+    window.scrollTo(0, 2500);
+    behavior: 'smooth';
+  }
+
+  function scrollcontact() {
+    window.scrollTo(0, 1800);
+    behavior: 'smooth';
+  }
